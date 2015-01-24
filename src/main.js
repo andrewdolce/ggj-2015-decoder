de140615111d227df9c830b75a64d0839482f802
Eric Li <li.zhe.hua@gmail.com>
2015-01-24 10:14:59 -0500

    Add better initialization
    Create missing retarget root if necessary.
    Cache retarget root to reduce runtime lookups.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,38 @@
 (function() {
   $('#hand span, #board span').addClass('dc-card');
-  $('#hand, #board').addClass('dc-deck');
+  $('#hand, #board')
+    .addClass('dc-deck-container')
+    .each(function() {
+      var retargetRootSelector = 'p:first-of-type';
+      var $retargetRoot = $(this).find(retargetRootSelector);
+      if (!$retargetRoot.length) {
+        $retargetRoot = $('<p>').appendTo(this);
+      }
+      $retargetRoot.addClass('dc-deck-root');
+      this.$__retargetRoot = $retargetRoot;
+    });
+
+  // Clean out any extraneous text nodes between cards.
+  $('.dc-deck-container .dc-deck-root')
+    .contents()
+    .filter(function() {
+      return this.nodeType === 3; // TEXT_NODE
+    })
+    .remove();
 
   $('.dc-card').draggable({
     revert: true,
     revertDuration: 300
   });
 
-  $('.dc-deck').droppable({
+  $('.dc-deck-container').droppable({
     accept: '.dc-card',
     drop: function(ev, ui) {
-      var $p = $(this).find('p:first-of-type');
       ui.draggable
-        .appendTo($p)
+        .appendTo(this.$__retargetRoot)
         // Reposition in "local space" to match previous "world space" offset
         .offset(ui.offset);
     }
   });
+
 }());
